9e884bfcb67c7ebc5fd003c122c61c38e1258ba8
William Breathitt Gray <vilhelm.gray@gmail.com>
2015-08-08 21:54:53 -0400

    Initial commit

---
+++ src/MissionAccomplished.cpp
@@ -0,0 +1,31 @@
+/* Copyright (C) 2015 William Breathitt Gray
+ *
+ * This file is part of Mission Accomplished.
+ *
+ * Mission Accomplished is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Mission Accomplished is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Mission Accomplished.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+#include <iostream>
+
+#include "SDL.h"
+
+int main(){
+        if (SDL_Init(SDL_INIT_VIDEO)){
+                std::cerr << SDL_GetError() << std::endl;
+                return 1;
+        }
+
+        SDL_Quit();
+        return 0;
+}
